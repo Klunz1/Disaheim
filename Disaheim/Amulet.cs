6426742cf4b8c7742d392617cde20a1b5fc9de8f
Klunz <Klunzrlive.dk>
2023-10-24 14:53:12 +0200

    disaheim

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+        //properties implementeres
+        public string ItemId { get; set; }
+        public string Design { get; set; }
+        public Level Quality { get; set; }
+
+        //constructors implementeres
+        public Amulet(string itemId)
+        {
+            this.ItemId = itemId;
+        }
+
+        public Amulet(string itemId, Level quality)
+            :this(itemId) 
+        {
+            this.Quality = quality;
+        }
+
+        public Amulet(string itemId, Level quality, string design)
+            :this(itemId, quality) 
+        {
+            this.Design = design;
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Quality: {Quality}, Design: {Design}";
+        }
+
+    }
+}
