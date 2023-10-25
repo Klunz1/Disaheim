596efcfd899d575e99ff0b08af79f9c779d83937
Klunz <Klunz@live.dk>
2023-10-25 10:52:03 +0200

    nu jeg færdig

--- Disaheim/Controller.cs
+++ Disaheim/Controller.cs
@@ -9,17 +9,19 @@ namespace Disaheim
     public class Controller
     {
         
+        //properties
         public List<Book> Books { get; set; }
 
         public List<Amulet> Amulets { get; set; }
-
+        
+        //Initialiser properties i constructor
         public Controller()
         {
             Books = new List<Book>();
             Amulets = new List<Amulet>();
 
         }
-        
+        //de to metoder der tilføjer bøger og amuletter
         public void AddToList(Book book) 
         { 
             Books.Add(book);
