fb640dbc6b7041295c671896fe7a90b9cd57265f
Klunz <Klunz@live.dk>
2023-11-15 08:34:56 +0100

    hej

---
+++ Disaheim/Utility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Disaheim;
+
+
+namespace Disaheim
+{
+    public class Utility
+    {
+        public double LowQualityValue { get; set; } = 12.5;
+        public double MediumQualityValue { get; set; } = 20.00;
+        public double HighQualityValue { get; set; } = 27.5;
+
+        public double CourseHourValue { get; set; } = 875.00;
+        //metoder oprettes
+
+        public double GetValueOfMerchandise(Merchandise merchandise)
+        {
+            
+            if (merchandise is Amulet amulet )
+            {
+                double value = 0;
+                Utility utility = new Utility();
+
+                if (amulet.Quality == Level.low)
+                {
+                   value = utility.LowQualityValue;
+                }
+                if (amulet.Quality == Level.medium)
+                {
+                    value = utility.MediumQualityValue;
+                }
+                if (amulet.Quality == Level.high)
+                {
+                    value = utility.HighQualityValue;
+                }
+                return value;
+            }
+            if (merchandise is Book book ) 
+            {
+                return book.Price;
+            }
+            return 0;
+        }
+        
+        public double GetValueOfCourse(Course course) 
+        {
+            double courseCost = 0;
+            double restCost=0;
+            Utility util= new Utility();
+
+            int fullHourCost = course.DurationInMinutes / 60;
+            
+            if (course.DurationInMinutes % 60 > 0)
+            {
+                restCost = util.CourseHourValue;
+            }
+
+            courseCost = (fullHourCost * util.CourseHourValue) + restCost;
+            return courseCost;
+        }
+    }
+}
---
+++ Pr24-WPFInteractiveGUI/WPFInteractiveGUI/WPFInteractiveGUI/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFInteractiveGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Controller controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new Controller();
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            lblpersonCount.Content = "Person Count: " + controller.PersonCount;
+            lblindex.Content = "Index: " + controller.PersonIndex;
+
+            UpdateControlsAvailability();
+
+            if (controller.CurrentPerson != null)
+            {
+                tbfirstName.Text = controller.CurrentPerson.FirstName;
+                tblastName.Text = controller.CurrentPerson.LastName;                
+                tbage.Text = controller.CurrentPerson.Age.ToString();
+                tbtelephoneNo.Text = controller.CurrentPerson.TelephoneNo;
+            }
+            else
+            {
+                ClearPersonDetails();
+            }
+        }
+
+        private void UpdateControlsAvailability()
+        {
+            bool hasPersons = controller.PersonCount != 0;
+
+            tbfirstName.IsEnabled = hasPersons;
+            tblastName.IsEnabled = hasPersons;
+            tbage.IsEnabled = hasPersons;
+            tbtelephoneNo.IsEnabled = hasPersons;
+
+            btndeletePerson.IsEnabled = hasPersons;
+            btnup.IsEnabled = hasPersons && controller.PersonCount > 1;
+            btndown.IsEnabled = hasPersons && controller.PersonCount > 1;
+        }
+
+        private void ClearPersonDetails()
+        {
+            tbfirstName.Text = "";
+            tblastName.Text = "";           
+            tbage.Text = "";
+            tbtelephoneNo.Text = "";
+        }
+
+        private void btnnewPerson_Click(object sender, RoutedEventArgs e)
+        {
+            controller.AddPerson();
+            UpdateUI();
+            // Opdater alder kun hvis tbage er aktiveret
+            
+        }
+
+        private void btndeletePerson_Click(object sender, RoutedEventArgs e)
+        {
+            controller.DeletePerson();
+            UpdateUI();
+        }
+
+        private void btnup_Click(object sender, RoutedEventArgs e)
+        {
+            controller.NextPerson();
+            // Opdater alder kun hvis tbage er aktiveret
+           
+            UpdateUI();
+        }
+
+        private void btndown_Click(object sender, RoutedEventArgs e)
+        {
+            controller.PrevPerson();
+            // Opdater alder kun hvis tbage er aktiveret
+           
+            UpdateUI();
+        }
+
+        private void tbfirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            controller.CurrentPerson.FirstName = tbfirstName.Text;
+        }
+
+        private void tblastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            controller.CurrentPerson.LastName = tblastName.Text;
+        }
+
+        private void tbage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string intAge = tbage.Text;
+            if (int.TryParse(intAge, out int parsedAge))
+            {
+                controller.CurrentPerson.Age = parsedAge;
+            }
+            else
+            {
+                // Behandl ugyldig indtastning her, f.eks. vis en meddelelse til brugeren.
+            }
+        }
+
+        private void tbtelephoneNo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            controller.CurrentPerson.TelephoneNo = tbtelephoneNo.Text;
+        }
+    }
+}
